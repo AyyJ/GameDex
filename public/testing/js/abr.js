944844b660b0c1da117dd8e246f622d0b5f61a04
Brian Maier <bkmaier@ucsd.edu>
2017-03-04 13:45:29 -0800

    Created external JS file, cleaned up JS code, added wrapper object

--- public/testing/js/abr.js
+++ public/testing/js/abr.js
@@ -1,1 +1,50 @@
-var abr = {}; // Wrapper object. Named for Arthur, Brian, & Rabab
+/*
+ * Wrapper object. Named for Arthur, Brian, & Rabab.
+ */
+var abr = {};
+
+
+/*
+ * Function: Initializes the registration page.
+ */
+abr.initRegisterPage = function() {
+   document.getElementById('register').addEventListener('click', abr.registerNewUser, false);
+}
+
+
+/*
+ * Function: Register a new user.
+ */
+abr.registerNewUser = function() {
+   abr.createFirebaseUser();
+   firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+         abr.createGamedexUser();
+         window.location.replace('viewlist.html');
+      }
+   });
+}
+
+
+/* Function: Creates the Firebase user. */
+abr.createFirebaseUser = function() {
+   var email = document.getElementById('email').value;
+   var password = document.getElementById('password').value;
+
+   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+         alert('The password is too weak.');
+      } else {
+         alert(errorMessage);
+      }
+      console.log(error);
+   });
+}
+
+
+/* Function: Add the custom user information. */
+abr.createGamedexUser = function() {
+
+}
