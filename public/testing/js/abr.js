ef82bba595147ac71202d6a481dc4b461c446383
Brian Maier <bkmaier@ucsd.edu>
2017-03-04 15:34:14 -0800

    Moved JS to external file, cleaned up JS code, fixed listener error

--- public/testing/js/abr.js
+++ public/testing/js/abr.js
@@ -5,17 +5,81 @@ var abr = {};
 
 
 /*
+ * Function: Initializes the index page.
+ */
+abr.initIndexPage = function() {
+   firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+         window.location.replace("viewlist.html");
+      }
+   });
+   document.getElementById('login').addEventListener('click', abr.handleLogin, false);
+   document.getElementById('googleLogin').addEventListener('click', abr.handleGoogleLogin, false);
+}
+
+
+/*
+ * Function: Processes the login request.
+ */
+abr.handleLogin = function() {
+   var email = document.getElementById('email').value;
+   var password = document.getElementById('password').value;
+
+   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+         alert('Wrong password.');
+      } else {
+         alert(errorMessage);
+      }
+      console.log(error);
+   });
+}
+
+
+/*
+ * Function: Processes the Google login request.
+ */
+ abr.handleGoogleLogin = function() {
+    if (!firebase.auth().currentUser) {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+         var user = result.user;
+
+      }).catch(function(error) {
+         // Handle Errors here.
+         var errorCode = error.code;
+         var errorMessage = error.message;
+         // The email of the user's account used.
+         var email = error.email;
+         // The firebase.auth.AuthCredential type that was used.
+         var credential = error.credential;
+         if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+            // If you are using multiple auth providers on your app you should handle linking
+            // the user's accounts here.
+         } else {
+            console.error(error);
+         }
+      });
+   }
+}
+
+
+/*
  * Function: Initializes the registration page.
  */
 abr.initRegisterPage = function() {
-   document.getElementById('register').addEventListener('click', abr.registerNewUser, false);
+   document.getElementById('register').addEventListener('click', abr.handleRegistration, false);
 }
 
 
 /*
  * Function: Register a new user.
  */
-abr.registerNewUser = function() {
+abr.handleRegistration = function() {
    abr.createFirebaseUser();
    firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
@@ -26,7 +90,9 @@ abr.registerNewUser = function() {
 }
 
 
-/* Function: Creates the Firebase user. */
+/*
+ * Function: Adds the user account information to the Firebase database.
+ */
 abr.createFirebaseUser = function() {
    var email = document.getElementById('email').value;
    var password = document.getElementById('password').value;
@@ -44,7 +110,9 @@ abr.createFirebaseUser = function() {
 }
 
 
-/* Function: Add the custom user information. */
+/*
+ * Function: Add the custom user information.
+ */
 abr.createGamedexUser = function() {
    var displayName = document.getElementById('displayName').value;
 
