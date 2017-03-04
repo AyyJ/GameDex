c366027a534b2c28b3a7f1ddc3219a65e1dbaca0
Arthur de Jesus <adejesus@ucsd.edu>
2017-03-03 23:43:11 -0800

    worked on auth

---
+++ public/testing/js/base.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCf9mEv6fjrsEk9ZryrfGZv-ygiBhpVt1Y",
+    authDomain: "gamedex-2d485.firebaseapp.com",
+    databaseURL: "https://gamedex-2d485.firebaseio.com",
+    storageBucket: "gamedex-2d485.appspot.com",
+    messagingSenderId: "1057082821990"
+  };
+ 
+ firebase.initializeApp(config);
