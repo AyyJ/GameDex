07d20985831c496979103fd85187fa56f788ba3a
Brian Maier <bkmaier@ucsd.edu>
2017-03-06 10:44:50 -0800

    Added scaffolding for basic CRUD demo page

---
+++ public/demo/demo.js
@@ -0,0 +1,34 @@
+/*
+ * Firebase connection configuration.
+ */
+var config = {
+   apiKey: "AIzaSyCf9mEv6fjrsEk9ZryrfGZv-ygiBhpVt1Y",
+   authDomain: "gamedex-2d485.firebaseapp.com",
+   databaseURL: "https://gamedex-2d485.firebaseio.com",
+   storageBucket: "gamedex-2d485.appspot.com",
+   messagingSenderId: "1057082821990"
+};
+var app = firebase.initializeApp(config);
+var db = firebase.database()
+
+
+/*
+ * Wrapper object.
+ */
+var demo = {};
+
+
+/*
+ * Function: Initializes the SPA page.
+ */
+demo.initApp = function() {
+   document.getElementById('add_new_text').addEventListener('click', demo.handleAddTextButton, false);
+}
+
+
+/*
+ * Function: Processes the add text button.
+ */
+demo.handleAddTextButton = function() {
+   alert('Clicked!');
+}
